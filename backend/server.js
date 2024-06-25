a00d89e8b92d4ee9f79db20123c6a603175eee59
shriman <shriman.mogaveera@epsilon.com>
2024-06-25 12:27:45 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser"
+import path from "path";
 
 import authRoutes from "./routes/auth.router.js";
 import messageRoutes from "./routes/message.router.js";
@@ -12,6 +13,8 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); // 
@@ -21,9 +24,12 @@ app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
 
-// app.get("/",(req,res) => {
-//     res.send("hello ");
-// })
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+})
+
 
 server.listen(PORT, () => {
     connectToMongoDB();
