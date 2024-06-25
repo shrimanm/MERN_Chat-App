074d87162d56471105f6e4515aa2f23424c7c4ab
shriman <shriman.mogaveera@epsilon.com>
2024-06-25 14:47:31 +0530

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(() => {
         if(authUser){
-            const socket = io("http://localhost:5000",{
+            const socket = io("https://mern-chat-app-6l9f.onrender.com/",{
                 query:{
                     userId:authUser._id,
                 }
